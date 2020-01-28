c934477bdb64f2328103ae87679c3269ec543af5
Ingrid Cristina de Castro <ingridcris1995@gmail.com>
2020-01-27 23:51:17 -0300

    Testes.

---
+++ src/test/java/br/com/c2c/api/C2cApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.c2c.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2cApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
