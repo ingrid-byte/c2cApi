aa4641ed9aa29bcc3d0d88f8ec14acb2a437f969
Ingrid Cristina de Castro <ingridcris1995@gmail.com>
2020-01-27 23:46:21 -0300

    Classe que inicializa o spring.

---
+++ src/main/java/br/com/c2c/api/C2cApplication.java
@@ -0,0 +1,13 @@
+package br.com.c2c.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2cApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C2cApplication.class, args);
+    }
+
+}
