84efbcdd656107d18c06bf149f9da5debe5ce385
Ingrid Cristina de Castro <ingridcris1995@gmail.com>
2020-01-27 23:47:02 -0300

    Interface repository de produto, estende de JpaRepository e herda os principais metodos de um CRUD. Está definido que o retorno é um "Produto" e o id é um "Integer".

---
+++ src/main/java/br/com/c2c/api/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.c2c.api.repository;
+
+import br.com.c2c.api.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
